55deb2a5ba47e7d093f75983fcdb9a8dd4051b94
Douglas Uretsky <douglasuretsky@gmail.com>
2018-09-26 21:37:59 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
-  return  "Fizz" if num % 3 == 0
-  return 
+  return "Fizz" if num % 3 == 0
+  return "Buzz" if num % 5 == 0
 end
